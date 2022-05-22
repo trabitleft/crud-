48742619402a3e689c997717299d406ba4b5c825
cinta <cintaserasi3@gmail.com>
2022-05-22 11:41:49 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>Document</title>
+<style>
+    form {
+        width:100%;
+        height:100vh;
+    }
+</style>
+</head>
+<body>
+
+    
+    <form action="" method="post" class="container-fluid d-flex justify-content-center align-items-center">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Admin</h5>
+                <input type="text" class="form-control mb-2" id="exampleFormControlInput1" placeholder="name">
+                <input type="password" class="form-control mb-2" id="inputPassword" placeholder="password">
+                <a href="#" class="btn btn-primary">Login</a>
+            </div>
+        </div>
+    </form>
+
+ <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>   
+</body>
+</html>
